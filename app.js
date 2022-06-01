d4a29d648ec57ebc9b4c4be5768d7604e88b88f2
Juancito <cjuan.chona@hotmail.com>
2022-06-01 17:20:08 -0300

    añadi logica js solo falta mostrar lo de cada persona que fue guardado en un array faltaria poner que los campos deben estar completos y no ingresar numeros negativos

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const personas = []
 
 form.onsubmit=(e)=>{
     e.preventDefault()
-    const pago = form.pago
-    const nombre = form.nombre
+    const pago = Number(form.pago.value)
+    const nombre = form.nombre.value
     const totalp= $('#total-p')
+    const divPersonasPagando = $('#personas_pagando')
+    personas.push(Number(pago))
 
-    totalp.innerText=pago.value
+    divPersonasPagando.appendChild(personaInfoPago(nombre,pago))
+
+    totalp.innerText= Number(totalp.innerText) + pago
     const pagoPersona= $('#pago-cada-p')
+}
+
+function personaInfoPago(nombre,pago){
+    const div= document.createElement('div')
+    div.classList.add('px-4')
+    div.classList.add('border')
+    div.classList.add('rounded-bottom')
+    div.textContent=nombre+'   $'+pago
+    return div
 }
 
